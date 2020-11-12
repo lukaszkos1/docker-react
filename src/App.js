83bd41d47a6b7f5557ceb7024910c897ff2b51bd
TMS Brokers / Łukasz Kościesza <lk@tms.local>
2020-11-12 18:16:37 +0100

    Dodalem mala zmiane

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Cos duzo bardzo innego
+          Edit <code>src/App.js</code> and save to reload. Cos nowego i bardzo innego
         </p>
         <a
           className="App-link"
